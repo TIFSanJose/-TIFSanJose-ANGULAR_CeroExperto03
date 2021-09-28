95e6cbd164d476d2480f11564c847828bfaf6fbf
ramon <ramon@ramon>
2021-09-28 15:29:52 -0300

    v56 componente Heroes, dando estilos con cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./components/about/about.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HomeComponent } from "./components/home/home.component";
+
 
 const APP_ROUTE: Routes = [
     {path: 'home', component: HomeComponent },
+    {path: 'about', component: AboutComponent },
+    {path: 'heroes', component: HeroesComponent },
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 
 ];
