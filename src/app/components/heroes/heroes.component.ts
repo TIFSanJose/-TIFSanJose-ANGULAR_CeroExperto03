2be3b8e43321a5e8fbb57e5a646409ce2427b209
ramon <ramon@ramon>
2021-09-29 14:51:33 -0300

    v60 redireccion por funcion onClick o por routerLink

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { HeroesService, Herores } from 'src/app/services/heroes.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +12,16 @@ export class HeroesComponent implements OnInit {
   
   public heroes : Herores[] = []
 
-  constructor(private _heroesService: HeroesService) { 
-    console.log('soy constructor');
-    
+  constructor(  private _heroesService: HeroesService, 
+                private _router: Router               ) { 
   }
 
   ngOnInit(): void {
-    
     this.heroes = this._heroesService.getHeroes()
-    
-    console.log(this.heroes);
+  }
+
+  verHeroe(idx: number){
+    this._router.navigate(['heroe', idx]);
   }
 
 }
